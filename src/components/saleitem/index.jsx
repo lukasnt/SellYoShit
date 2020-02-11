d2fbb45c7371fe185c468dacebf89466d8b89374
Håvard <havhje@gmail.com>
2020-02-11 22:06:02 +0100

    #1 - Added saleitem list

---
+++ src/components/saleitem/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  media: {
+    height: 140
+  }
+});
+
+export default function SaleItem() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://www.ikea.com/no/no/images/products/landskrona-3-seat-sofa-gunnared-dark-grey-metal__0602115_PE680184_S5.JPG?f=s"
+          title="Sofa til salgs"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Sofa til salgs
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            499,-
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
