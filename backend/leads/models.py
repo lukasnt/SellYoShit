bacdba27184aa0d4caa3a48a6dcb33f242120a3d
sarunasj02 <43404375+sarunasj02@users.noreply.github.com>
2020-02-12 10:40:16 +0100

    #1 fix  venv + django setup

--- backend/leads/models.py
+++ backend/leads/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class User(models.model):
+class User(models.Model):
     username = models.CharField(max_length=16)
     password = models.CharField(max_length=16)##edit to not store raw text later on
     email = models.EmailField
     phone_num = models.IntegerField(max_length=10)
     first_name = models.CharField(max_length=16)
     last_name = models.CharField(max_length=16)
-    created_on = 
+    created_on = models.DateTimeField(auto_now_add=True)
 
 
-class Ad()
+##class Ad()
